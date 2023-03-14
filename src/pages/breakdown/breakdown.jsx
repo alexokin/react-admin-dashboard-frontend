e2ad1f987ccdf836405653daeddb645ff396d636
alexokin <alex.okin@gmail.com>
2023-03-14 17:29:24 +0200

    finished with breakdown chart

--- src/pages/breakdown/breakdown.jsx
+++ src/pages/breakdown/breakdown.jsx
@@ -1,8 +1,17 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import BreakdownChart from '../../cmps/breakdown-chart'
+import Header from '../../cmps/header'
 
 function Breakdown() {
+
   return (
-    <div>Breakdown</div>
+    <Box m="1.5rem 2.5rem">
+        <Header title="Breakdown" subtitle='Breakdown of Sales By Category'/>
+        <Box mt="40px" height="75vh">
+            <BreakdownChart />
+        </Box>
+    </Box>
   )
 }
 
